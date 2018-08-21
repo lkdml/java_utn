44cf06a207b59a9ce1fa2f0c69163ba8b80b1c66
Mariano Lopez Senes <marianolopezsenes@gmail.com>
2018-08-20 23:48:53 -0300

    Primer commit

---
+++ PrimerClase/src/Main.java
@@ -0,0 +1,32 @@
+
+public class Main {
+	
+	public static void main (String[] string) {
+		Persona p1 = new Persona();
+		
+		
+		p1.setNombre("Mariano");
+		p1.setApellido("Lopez");
+		p1.setDni(29232230);
+		
+		Persona p2 = new Persona(p1);
+		
+		
+		
+		if (p1.equals(p2)) {
+		System.out.println(p1.equals(p2));
+		} else {
+		
+		System.out.println(p1.toString());
+		}
+		
+		
+		
+		int a = 5;
+		int b = 0;
+		for (int i = 0; i <= 5; i++) {
+			b += i*2;
+		}
+	}
+
+}
---
+++ PrimerClase/src/Persona.java
@@ -0,0 +1,108 @@
+
+public class Persona {
+	
+	private String nombre;
+	private String apellido;
+	private String telefono;
+	private Integer dni;
+	
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public Integer getDni() {
+		return dni;
+	}
+
+	public void setDni(Integer dni) {
+		this.dni = dni;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getNombre() { 
+		return this.nombre;
+	}
+	
+	public Persona() {
+		super();
+	}
+	
+	public Persona (Persona pers) {
+		this.nombre = pers.nombre;
+		this.apellido = pers.apellido;
+		this.telefono = pers.telefono;
+		this.dni = pers.dni;
+		
+	}
+
+	@Override
+	public String toString() {
+		return "persona [nombre=" + nombre + ", apellido=" + apellido + ", telefono=" + telefono + ", dni=" + dni + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((apellido == null) ? 0 : apellido.hashCode());
+		result = prime * result + ((dni == null) ? 0 : dni.hashCode());
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		result = prime * result + ((telefono == null) ? 0 : telefono.hashCode());
+		return result;
+	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Persona other = (Persona) obj;
+		if (apellido == null) {
+			if (other.apellido != null)
+				return false;
+		} else if (!apellido.equals(other.apellido))
+			return false;
+		if (dni == null) {
+			if (other.dni != null)
+				return false;
+		} else if (!dni.equals(other.dni))
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		if (telefono == null) {
+			if (other.telefono != null)
+				return false;
+		} else if (!telefono.equals(other.telefono))
+			return false;
+		return true;
+	}
+	
+	
+
+	
+	
+
+}
